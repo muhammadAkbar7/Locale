3f4ea5cf84d944f92eb116752c080b59d775a646
Muhammad Akbar <muhammadakbarwork@gmail.com>
2024-04-16 23:28:11 -0700

    Added profile page and initial reward page

---
+++ locale-app/locale-app/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController.swift
+//  locale-app
+//
+//  Created by Muhammad Akbar on 4/16/24.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ locale-app/locale-app/RewardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RewardViewController.swift
+//  locale-app
+//
+//  Created by Muhammad Akbar on 4/16/24.
+//
+
+import Foundation
+import UIKit
+
+class RewardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
